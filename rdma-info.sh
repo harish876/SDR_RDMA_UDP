95f108df8bd750f95011a0815f99d55419e34ef7
drdavis65 <daltonrdavis92@gmail.com>
2025-10-23 18:01:55 -0400

    separate tool install and info scripts

--- rdma-info.sh
+++ rdma-info.sh
@@ -1,11 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-
-# install RDMA userspace & NIC utilities
-sudo apt update
-sudo apt install -y rdma-core ibverbs-providers perftest mstflint ethtool iperf3 ibverbs-utils
-
 # show mellanox PCI devices
 lspci | grep -iE 'mellanox|connectx|mlx' || true
 
